6031c1d8b3e284075ca88dd5c0e2803e2b5ed100
lch-howger <360000521@qq.com>
2021-08-11 16:50:30 +0800

    format

--- 16_test_light_year/test05_fight.sol
+++ 16_test_light_year/test05_fight.sol
@@ -19,7 +19,7 @@ contract LightYearTest {
         Ship memory attacker = Ship(100, 100, 100, 100, 100);
         Ship memory defender = Ship(100, 100, 100, 100, 100);
 
-        bytes memory battleBytes="";
+        bytes memory battleBytes = "";
 
         uint256 count = 0;
         while (true) {
@@ -29,30 +29,30 @@ contract LightYearTest {
             }
 
             uint16 damage = _attack(attacker, defender);
-            defender.health=_causeDamage(defender.health,damage);
+            defender.health = _causeDamage(defender.health, damage);
 
-           battleBytes= _addBytes(battleBytes,defender.health);
+            battleBytes = _addBytes(battleBytes, defender.health);
 
-            if(defender.health<=0){
+            if (defender.health <= 0) {
                 break;
             }
 
-             damage = _attack(defender, attacker);
-            attacker.health=_causeDamage(attacker.health,damage);
+            damage = _attack(defender, attacker);
+            attacker.health = _causeDamage(attacker.health, damage);
 
-           battleBytes=  _addBytes(battleBytes,attacker.health);
+            battleBytes = _addBytes(battleBytes, attacker.health);
 
-            if(attacker.health<=0){
+            if (attacker.health <= 0) {
                 break;
             }
-            
+
         }
-        
+
         return battleBytes;
     }
 
-    function _addBytes(bytes memory b,uint16 i)private pure returns(bytes memory){
-       return  _mergeBytes(b,abi.encodePacked(i));
+    function _addBytes(bytes memory b, uint16 i) private pure returns (bytes memory){
+        return _mergeBytes(b, abi.encodePacked(i));
     }
 
 
@@ -67,28 +67,28 @@ contract LightYearTest {
 
     }
 
-    function _causeDamage(uint16 health,uint16 damage) private pure returns (uint16){
-       if(health<=damage){
-           return 0;
-       }else{
-           return health-damage;
-       }
+    function _causeDamage(uint16 health, uint16 damage) private pure returns (uint16){
+        if (health <= damage) {
+            return 0;
+        } else {
+            return health - damage;
+        }
     }
 
-function _mergeBytes(bytes memory a, bytes memory b) private pure returns (bytes memory c) {
-    uint alen = a.length;
-    uint totallen = alen + b.length;
-    uint loopsa = (a.length + 31) / 32;
-    uint loopsb = (b.length + 31) / 32;
-    assembly {
-        let m := mload(0x40)
-        mstore(m, totallen)
-        for {  let i := 0 } lt(i, loopsa) { i := add(1, i) } { mstore(add(m, mul(32, add(1, i))), mload(add(a, mul(32, add(1, i))))) }
-        for {  let i := 0 } lt(i, loopsb) { i := add(1, i) } { mstore(add(m, add(mul(32, add(1, i)), alen)), mload(add(b, mul(32, add(1, i))))) }
-        mstore(0x40, add(m, add(32, totallen)))
-        c := m
+    function _mergeBytes(bytes memory a, bytes memory b) private pure returns (bytes memory c) {
+        uint alen = a.length;
+        uint totallen = alen + b.length;
+        uint loopsa = (a.length + 31) / 32;
+        uint loopsb = (b.length + 31) / 32;
+        assembly {
+            let m := mload(0x40)
+            mstore(m, totallen)
+            for {let i := 0} lt(i, loopsa) {i := add(1, i)} {mstore(add(m, mul(32, add(1, i))), mload(add(a, mul(32, add(1, i)))))}
+            for {let i := 0} lt(i, loopsb) {i := add(1, i)} {mstore(add(m, add(mul(32, add(1, i)), alen)), mload(add(b, mul(32, add(1, i)))))}
+            mstore(0x40, add(m, add(32, totallen)))
+            c := m
+        }
     }
-}
 
 }
 
