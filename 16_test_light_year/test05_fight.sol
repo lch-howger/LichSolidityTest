bc3c7443d57469697cae2162ce972e8f894d2911
lch-howger <360000521@qq.com>
2021-08-11 16:49:40 +0800

    update pure battle

--- 16_test_light_year/test05_fight.sol
+++ 16_test_light_year/test05_fight.sol
@@ -15,12 +15,11 @@ contract LightYearTest {
         uint16 health;
     }
 
-    bytes battleBytes;
-    uint16[]  battleArr;
-
-    function battle() public returns (bytes memory){
+    function pureBattle() public pure returns (bytes memory){
         Ship memory attacker = Ship(100, 100, 100, 100, 100);
         Ship memory defender = Ship(100, 100, 100, 100, 100);
+
+        bytes memory battleBytes="";
 
         uint256 count = 0;
         while (true) {
@@ -30,18 +29,18 @@ contract LightYearTest {
             }
 
             uint16 damage = _attack(attacker, defender);
-            defender.health=causeDamage(defender.health,damage);
+            defender.health=_causeDamage(defender.health,damage);
 
-            push(defender.health);
+           battleBytes= _addBytes(battleBytes,defender.health);
 
             if(defender.health<=0){
                 break;
             }
 
              damage = _attack(defender, attacker);
-            attacker.health=causeDamage(attacker.health,damage);
+            attacker.health=_causeDamage(attacker.health,damage);
 
-            push(attacker.health);
+           battleBytes=  _addBytes(battleBytes,attacker.health);
 
             if(attacker.health<=0){
                 break;
@@ -49,90 +48,26 @@ contract LightYearTest {
             
         }
         
-        bytes memory b=new bytes(battleArr.length);
-        for(uint i=0; i<battleArr.length;i++){
-           
-        }
-        
-        return b;
-    }
-
-    function pureBattle() public  returns (bytes memory){
-        Ship memory attacker = Ship(100, 100, 100, 100, 100);
-        Ship memory defender = Ship(100, 100, 100, 100, 100);
-
-        uint256 count = 0;
-        while (true) {
-            count++;
-            if (count >= 20) {
-                break;
-            }
-
-            uint16 damage = _attack(attacker, defender);
-            defender.health=causeDamage(defender.health,damage);
-
-            push(defender.health);
-
-            if(defender.health<=0){
-                break;
-            }
-
-             damage = _attack(defender, attacker);
-            attacker.health=causeDamage(attacker.health,damage);
-
-            push(attacker.health);
-
-            if(attacker.health<=0){
-                break;
-            }
-            
-        }
-        
-        bytes memory b=new bytes(battleArr.length);
-        for(uint i=0; i<battleArr.length;i++){
-           
-        }
-        
-        return b;
-    }
-
-    function getBattleArr() public view returns(uint16[] memory){
-        return battleArr;
-    }
-
-    function getBattleBytes() public view returns(bytes memory){
         return battleBytes;
     }
 
-    function push(uint16 i) private{
-        battleArr.push(i);
-      battleBytes=  MergeBytes(battleBytes,abi.encodePacked(i));
+    function _addBytes(bytes memory b,uint16 i)private pure returns(bytes memory){
+       return  _mergeBytes(b,abi.encodePacked(i));
     }
 
 
-
-    function _attack(Ship memory attacker, Ship memory defender) private returns (uint16){
+    function _attack(Ship memory attacker, Ship memory defender) private pure returns (uint16){
         uint16 damage = attacker.attack + attacker.attack * attacker.accuracy / (attacker.attack + attacker.accuracy);
-push(damage);
+
         uint16 resist = defender.defense + defender.defense * defender.agility / (defender.defense + defender.agility);
-push(resist);
+
         uint16 realDamage = damage * damage / (damage + resist);
-    push(realDamage);
 
-        return (realDamage);
+        return realDamage;
 
     }
 
-    function getBytes() public pure returns (bytes memory){
-        //bytes memory b = new bytes(i);
-        //bytes memory b=abi.encodePacked(i);
-        bytes memory a="aaa";
-        bytes memory b="bbb";
-       bytes memory c= MergeBytes(a,b);
-        return c;
-    }
-
-    function causeDamage(uint16 health,uint16 damage) public pure returns (uint16){
+    function _causeDamage(uint16 health,uint16 damage) private pure returns (uint16){
        if(health<=damage){
            return 0;
        }else{
@@ -140,7 +75,7 @@ push(resist);
        }
     }
 
-function MergeBytes(bytes memory a, bytes memory b) public pure returns (bytes memory c) {
+function _mergeBytes(bytes memory a, bytes memory b) private pure returns (bytes memory c) {
     uint alen = a.length;
     uint totallen = alen + b.length;
     uint loopsa = (a.length + 31) / 32;
