cd74a7a62ae53d9a0cfe344a173c8c7614b05cfa
lch-howger <360000521@qq.com>
2021-08-16 14:02:35 +0800

    Update test04_test.sol

--- 19_test_light_year/test04_test.sol
+++ 19_test_light_year/test04_test.sol
@@ -6,18 +6,30 @@ contract Storage {
     function getRandomNumbers() public view returns(uint256[] memory){
         uint256[] memory arr=new uint256[](10);
         for(uint i=0;i<10;i++){
-            arr[i]=_random(3);
+            arr[i]=_viewRandom(3);
         }
         return arr;
     }
     
-    function _random(uint256 randomSize) private view returns(uint256){
+    function _viewRandom(uint256 randomSize) private view returns(uint256){
         uint256 difficulty=block.difficulty;
         uint256 gaslimit=block.gaslimit;
         uint256 number=block.number;
         uint256 timestamp=block.timestamp;
         uint256 gasprice=tx.gasprice;
         uint256 random = uint256(keccak256(abi.encodePacked(difficulty,gaslimit,number,timestamp,gasprice))) % randomSize;
+        return random;
+    }
+    
+    uint256 count;
+    function _random(uint256 randomSize) private returns(uint256){
+        count++;
+        uint256 difficulty=block.difficulty;
+        uint256 gaslimit=block.gaslimit;
+        uint256 number=block.number;
+        uint256 timestamp=block.timestamp;
+        uint256 gasprice=tx.gasprice;
+        uint256 random = uint256(keccak256(abi.encodePacked(count,difficulty,gaslimit,number,timestamp,gasprice))) % randomSize;
         return random;
     }
 }
