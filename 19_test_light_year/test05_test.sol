ab8e331e632091589b61870268eda6a8f28d0928
lch-howger <360000521@qq.com>
2021-08-16 15:08:49 +0800

    Create test05_test.sol

---
+++ 19_test_light_year/test05_test.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+contract Storage {
+ 
+    function getBytes() public pure returns(bytes memory){
+        bytes1  a=0x01;
+        bytes1  b=0x02;
+        bytes memory c="aabb";
+        uint16 d=1;
+        bytes memory result=abi.encodePacked(a,b,c,d);
+        return result;
+    }
+    
+}
